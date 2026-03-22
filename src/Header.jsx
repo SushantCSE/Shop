55ba7c314b68da51d0b3c868fa307fda4be444ba
sushant <khandekarsushant5@gmail.com>
2026-03-22 14:13:46 +0530

    add

---
+++ src/Header.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Header from './Header'
+
+function Header() {
+
+    <div>
+    <h1 className='text-blue-500 text-3xl font-bold text-center'>We are learning tailwindcss</h1>
+    </div>
+
+  return (
+    <header className="p-4 bg-gray-800 text-white">
+      <h1 className="text-2xl font-bold">My App</h1>
+    </header>
+  )
+}
+
+export default Header;
